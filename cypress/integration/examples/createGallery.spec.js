43c049b9229dc5924d76dde058a79056c6bddbd1
mtodmarkotod <mtod.markotod@gmail.com>
2021-05-24 18:25:28 +0200

    pull commit

--- cypress/integration/examples/createGallery.spec.js
+++ cypress/integration/examples/createGallery.spec.js
@@ -1,19 +1,19 @@
-import {createGallery} from "../../pageObjects/createGallery"
 const locators = require ("../../fixtures/lokators.json");
+import {authLogin} from "../../pageObjects/loginPage"
+import {createGallery} from "../../pageObjects/createGallery"
 
-describe('login', () => {
 
     beforeEach(() => {
         cy.visit('');
         cy.get(locators.navigation.loginBtn).eq(0).click();
 
     })
-
     it('Create Gallery', () => {
-              
+
+        authLogin.login('mtod.markotod@gmail.com', 'Maremare2')             
         createGallery.clickCreateGalleryLink()
    
     })
 
 
-})
+
